8105e840ff0c12479d58b06299919bb0f92cf091
Kevin M <k.mary@live.be>
2024-06-27 11:35:09 +0200

    bug correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
   users = DUMMY_USERS;
